cb995317b2e0699e7974deb2bd6a43ecf7ea384a
Xiaoou Zhang <kepbod@gmail.com>
2014-08-19 13:19:58 +0800

    first init

---
+++ README.md
@@ -0,0 +1,8 @@
+interval -- Python package dealing with intervals
+-------------------------------------------------
+
+License
+-------
+
+Copyright (C) 2012-2014 Xiao-Ou Zhang and contributors. See the (LICENSE)[https://github.com/kepbod/interval/blob/master/LICENSE.txt] file for
+license rights and limitations (MIT)
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='interval',
+      version='1.0',
+      description='Deal with intervals.',
+      author='Xiao-Ou Zhang',
+      author_email='kepbod@gmail.com',
+      maintainer='Xiao-Ou Zhang',
+      maintainer_email='kepbod@gmail.com',
+      license='MIT',
+      py_modules=['interval']
+      )
